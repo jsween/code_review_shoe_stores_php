6f66038c72005ed1e84bb15c23f78217a1fc7243
Jon <jonsweeney26@gmail.com>
2016-03-07 10:21:12 -0800

    add test conditions for getters

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -15,7 +15,35 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
-        
+        protected function tearDown()
+        {
+            // Brand::deleteAll();
+            // Store::deleteAll();
+        }
+
+        function test_getName()
+        {
+            // Arrange
+            $name = "Store";
+            $id = 1;
+            $test_Client = new Brand($name, $id);
+            // Act
+            $result = $test_Client->getName();
+            // Assert
+            $this->assertEquals("Store", $result);
+        }
+
+        function test_getId()
+        {
+            // Arrange
+            $name = "Store";
+            $id = 1;
+            $test_Client = new Brand($name, $id);
+            // Act
+            $result = $test_Client->getId();
+            // Assert
+            $this->assertEquals(1, $result);
+        }
     }
 
  ?>
