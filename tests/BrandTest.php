ebb2911a9b77410c5f840c8894f3f632405d8f1f
Jon <jonsweeney26@gmail.com>
2016-03-07 09:49:35 -0800

    create a brandtest file

---
+++ tests/BrandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once __DIR__ . '/../src/Brand.php';
+    // require_once __DIR__ . '/../src/Store.php';
+
+    $server = 'mysql:host=localhost;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+
+    }
+
+ ?>
