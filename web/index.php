ad2f4f8b782f49b74b2b0eb6f78e18abceae53aa
Jon <jonsweeney26@gmail.com>
2016-03-07 13:02:23 -0800

    fix spelling typo

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-    date_default_timezone_set('America/Los_Angeles'); 
+    date_default_timezone_set('America/Los_Angeles');
     $website = require_once __DIR__.'/../app/app.php';
     $website->run();
 ?>
