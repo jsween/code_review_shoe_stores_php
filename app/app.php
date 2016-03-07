24b0ae5a3112296aab39bc98604e7dbf90f7254a
Jon <jonsweeney26@gmail.com>
2016-03-07 13:11:53 -0800

    add routes for getting stores and brands

--- app/app.php
+++ app/app.php
@@ -20,7 +20,13 @@
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
 
+    $app->get("/brands", function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
 
     return $app;
  ?>
